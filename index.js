fbbbf0f4db4933f5480a03d5ce607931d6e4bcc5
Tony Yu <syu2@uoregon.edu>
2023-04-17 03:44:24 -0700

    stoped while adding inquirer

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-const inquirier = require('inquirer')
-const fs = require('fs')
+const inquirier = require("inquirer");
+const fs = require("fs");
+const { Triangle } = require("./lib/triangle");
+const { Circle } = require("./lib/circle");
+const { Square } = require("./lib/square");
+
+function write(answers) {}
+
+function prompt() {
+	inquirier.prompt([
+		{
+			type: "input",
+			name: "logo",
+			message: "What text do you want on your Logo?",
+			validate: (logo) => {
+				if (logo) {
+					return true;
+				} else {
+					console.log("need something special on your LOGO");
+				}
+			},
+		},
+		{
+            //test
+        },
+	]);
+}
