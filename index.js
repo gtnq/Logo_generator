0adef7f2a0bfdb56b105f70e9466695185b40bb2
Tony Yu <ttonyyu98@gmail.com>
2023-04-17 22:36:35 -0700

    completed

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const { Square } = require("./lib/square");
 const questions = [
 	{
 		type: "input",
-		name: "logo",
+		name: "text",
 		message: "What text do you want on your Logo?",
-		validate: (logo) => {
-			if (logo) {
+		validate: (text) => {
+			if (text) {
 				return true;
 			} else {
 				console.log("need something special on your LOGO");
@@ -18,22 +18,68 @@ const questions = [
 		},
 	},
 	{
-		type: "list",
-		name: "logo",
-		message: "What text do you want on your Logo?",
-		validate: (logo) => {
-			if (logo) {
+		type: "input",
+		name: "text_color",
+		message: "What color do you want for your text?",
+		validate: (text_color) => {
+			if (text_color) {
 				return true;
 			} else {
-				console.log("need something special on your LOGO");
+				console.log("Give me a color for the text please");
+			}
+		},
+	},
+	{
+		type: "list",
+		name: "logo",
+		message: "Pick your shape for the logo",
+		choices: ["Triangle", "Square", "Circle"],
+	},
+	{
+		type: "input",
+		name: "color",
+		message: "What color do you want for your logo?",
+		validate: (color) => {
+			if (color) {
+				return true;
+			} else {
+				console.log("Give me a color for the logo please");
 			}
 		},
 	},
 ];
 
-function generate(data) {}
+function generate(data) {
+	let string = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">`;
+	let text = `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${data.text_color}">${data.text}</text>`;
+	string += `<g>`;
+	string += `${data.logo}`;
+	let shape;
+	switch (data.logo) {
+		case "Triangle":
+			shape = new Triangle(data.color);
+			string += shape.render();
+			break;
+		case "Circle":
+			shape = new Circle(data.color);
+			string += shape.render();
+			break;
+		case "Square":
+			shape = new Square(data.color);
+			string += shape.render();
+			break;
+	}
 
-function write(data) {}
+	string += text;
+	string += `</g></svg>`;
+
+	write(string, data);
+}
+
+function write(string, data) {
+	let filename = `${data.text}_${data.text_color}.svg`;
+	fs.writeFile(`./exp/${filename}`, string, (err) => console.log(err));
+}
 
 function init() {
 	return inquirier.prompt(questions).then((data) => {
@@ -41,10 +87,7 @@ function init() {
 	});
 }
 
-init()
-	.then((userData) => {
-		return generate(data);
-	})
-	.then((text) => {
-		write(data);
-	});
+init().then((userData) => {
+	console.log(userData);
+	return generate(userData);
+});
